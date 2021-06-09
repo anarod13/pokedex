26c58ac8bcd771a9e78fc5e8d20894f25415d48b
anarod13 <anam.rodriguez13@gmail.com>
2021-06-09 16:58:08 -0300

    Armo tablero,agrego css, establezco índice y hago primera versión de ficha con detalles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+let $paginas = document.querySelectorAll(".pagina")
+let paginas = [];
+$paginas.forEach($pagina => paginas.push($pagina));
+const fichas = document.querySelectorAll(".pokeficha");
+let indice = document.querySelector("#indice");
+indice.addEventListener('click', seleccionarPagina, false);
+
 function asignarPokemon(ficha, i) {
     ++i;
     if (i < 899) {
@@ -7,29 +14,28 @@ function asignarPokemon(ficha, i) {
             .then(respuesta => {
                 ficha.querySelector(".pokenombre").innerText = respuesta.name;
                 ficha.querySelector(".pokefoto").src = respuesta.sprites.front_default;
+                ficha.id = respuesta.name;
+                ficha.querySelector(".pokenombre").id = respuesta.name;
+                ficha.querySelector(".pokefoto").id = respuesta.name;
+
             })
     } else {
-        limpiarFicha(ficha)
+        return limpiarFicha(ficha);
     }
 }
 
 
 armarTablero(0);
 
-let $paginas = document.querySelectorAll(".pagina")
-let indice = document.querySelector("#indice");
-let paginas = [];
-$paginas.forEach($pagina => paginas.push($pagina));
 
-indice.addEventListener('click', seleccionarPagina, false);
 
 function armarTablero(pagina) {
-
-    const fichas = document.querySelectorAll(".pokeficha");
     fichas.forEach(function(ficha, i) {
         let indice = chequearIndice(i, pagina)
         asignarPokemon(ficha, indice)
     })
+    document.querySelector("#info-pokemon").style.display = "none";
+    document.querySelector("#poketablero").addEventListener('click', mostrarPokemon, false)
 }
 
 function chequearIndice(i, pagina) {
@@ -45,4 +51,19 @@ function seleccionarPagina(paginaSeleccionada) {
 function limpiarFicha(ficha) {
     ficha.querySelector(".pokenombre").innerText = "";
     ficha.querySelector(".pokefoto").src = "";
+}
+
+function mostrarPokemon(ficha) {
+    document.querySelector("#info-pokemon").style.display = "block";
+    let pokemon = ficha.target.id;
+    fetch(` https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then(respuesta => respuesta.json())
+        .then(respuesta => {
+            document.querySelector("#nombre").innerText = respuesta.name;
+            document.querySelector("#numero").innerText = respuesta.id;
+            document.querySelector("#foto").src = respuesta.sprites.front_default;
+            document.querySelector("#peso").innerText = respuesta.weight;
+            document.querySelector("#altura").innerText = respuesta.height;
+        })
+    return;
 }
