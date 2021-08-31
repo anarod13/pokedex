8fe46dddabd55441df5c2510154bb598c7e12bbd
anarod13 <anam.rodriguez13@gmail.com>
2021-08-30 21:32:59 -0300

    Agrego localStorage

---
+++ src/localStorage.js
@@ -0,0 +1,9 @@
+export function cargarPokemon(pokemon) {
+    return JSON.parse(localStorage.getItem(pokemon));
+}
+
+
+export function guardarPokemon(pokemon, informacionPokemon) {
+    return localStorage.setItem(pokemon, informacionPokemon)
+
+}
